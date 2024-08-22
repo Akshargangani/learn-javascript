197aa372c67ebfcca8a834e9d3accc3c576cd337
garvit trivedi <garvit.trivedi.cg@gmail.com>
2024-08-22 14:09:35 +0530

    first commit

---
+++ array.js
@@ -0,0 +1,171 @@
+
+//  var nav_array=[1,"garvit ",34,26]
+//  console.log(nav_array[1])
+
+
+// 1. ARRAY LENGTH
+// {to find the how many things in array}
+// var fruits=["mango","banana","apple","orange "]
+// let size=fruits.length;
+// console.log(size)
+//output:4
+
+//2. array to sting 
+// {to remove the [] in the print like a normal without any brackets }
+// let array=["orange","mango","apple","banana"]
+// let a=array.toString()
+// console.log(a)
+//output:orange,mango,apple,banana
+
+
+// //3.array (array at) this methode same as[]
+// //(i) it use to print a one thing from the array.
+// let contain=["garvit","veer","shubham","lakshaya"]\
+// //function always contains ()brakest.
+// let b=contain.at(3) 
+// console.log(b)
+//output:lakshaya
+
+// (ii)let contain=["garvit","veer","shubham","lakshaya"]
+// // //function always contains ()brakest.
+ // console.log(contain[3])
+//output:lakshaya
+
+
+// //4. tojoin
+// //to add same thing between the array
+// let a=["garvit","DOB:26/03/2006","B.place:jalore"]
+// let b=a.join(" ## ")
+// console.log(b)
+// //output:garvit##DOB:26/0/2006##B.place:jalore
+
+//5.pop and pushing (remove and add from last)
+//pop means "out" and pushing means "into" ; mean when remove same thing use pop(out) annd to add same thing use pushing(into)..
+// let btn=["garvit","veer","shubham","lakshaya"]
+// btn.pop();
+// console.log(btn) 
+ 
+// let btn=["garvit","veer","shubham","lakshaya"]
+// btn.push("rajesh");
+// console.log(btn)
+
+
+// //6.shift and unshift(remove and add from the starting)
+// let array=["garvit","veer","shubham","lakshaya"]
+// let a= array.shift();
+// console.log(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
